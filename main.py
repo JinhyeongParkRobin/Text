899d8a1cb1eea76111869ce4ddd6759911ab6aef
SJA Student <sjastudent@bagjinhyeongs-MacBook-Air.local>
2024-03-30 17:09:00 +0900

    inisial push

---
+++ main.py
@@ -0,0 +1,10 @@
+1. Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+2. experimental, human, and random error
+4.In other languages, however, I've seen cases both where it doesn't matter (in JavaScript, both 'hi'=="hi" and 'hi'==="hi" return true) and where it does (in C#, "d" is a string while 'd' is a char)
+5.A string is a palindrome when it is the same
+when read backwards.
+For example:
+the string "bob" is a palindrome So is "abba".
+But the string "abcd" is not a palindrome, because "abcd" != "dcba".5.
